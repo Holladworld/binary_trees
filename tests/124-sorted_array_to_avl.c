9e315e6cf6a799278fbf44c09fe3e03be05a5470
aliyumusamkk <aliyumusakalakala@gmail.com>
2023-10-31 14:04:59 -0700

    binary_trees task

---
+++ tests/124-sorted_array_to_avl.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * create_tree - creates an AVL tree with recursion
+ *
+ * @node: pointer node
+ * @array: input array of integers
+ * @size: size of array
+ * @mode: 1 to adding on the left, 2 to adding on the right
+ * Return: no return
+ */
+void create_tree(avl_t **node, int *array, size_t size, int mode)
+{
+	size_t middle;
+
+	if (size == 0)
+		return;
+
+	middle = (size / 2);
+	middle = (size % 2 == 0) ? middle - 1 : middle;
+
+	if (mode == 1)
+	{
+		(*node)->left = binary_tree_node(*node, array[middle]);
+		create_tree(&((*node)->left), array, middle, 1);
+		create_tree(&((*node)->left), array + middle + 1, (size - 1 - middle), 2);
+	}
+	else
+	{
+		(*node)->right = binary_tree_node(*node, array[middle]);
+		create_tree(&((*node)->right), array, middle, 1);
+		create_tree(&((*node)->right), array + middle + 1, (size - 1 - middle), 2);
+	}
+}
